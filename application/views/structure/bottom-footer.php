111129a462e293a23335bfa84ad9c7671cb9a2a4
Cheke <xekevara@gmail.com>
2018-04-06 23:19:21 +0200

    Fix: Delete a residual file

--- application/views/structure/bottom-footer.php
+++
@@ -1,9 +0,0 @@
-<?php defined("BASEPATH") or exit("No direct script access allowed"); ?>
-
-<div class="container">
-    <div class="row">
-        <div class="align-middle text-center col-md-12">
-            <span class="line-height-40"> © 2017 - Todos los derechos reservados a FreeBird Entertainment System S.L. (FES).</span>
-        </div>
-    </div>
-</div>
